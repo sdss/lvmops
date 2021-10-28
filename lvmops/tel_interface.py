12a6e5b9e854588c29145a39d1856141992da453
ndrory <40801533+ndrory@users.noreply.github.com>
2021-10-28 12:12:59 -0500

    distinguish guide and pointing offsets

--- lvmops/tel_interface.py
+++ lvmops/tel_interface.py
@@ -21,50 +21,57 @@ class telescope(object):
     def goto_screen(self): 
         pass
 
+    # pointing offset: change track rates
     def offset(self, target=None, delta_ra=None, delta_dec=None, delta_x=None, delta_y=None):
         pass
     
+    # guiding offset: do NOT change track rates
+    def guide_offset(self, target=None, delta_ra=None, delta_dec=None, delta_x=None, delta_y=None):
+        pass
+    
     def paoffset(self, delta_PA=None):
-        '''
-        Apply a relative rotation offset by moving the K-mirror
-        
-        Parameters:
-            delta_PA (float): rotation offset in degrees (+/- = E/W)
-        
-        Returns:
-            None
-        '''
+    '''
+    Apply a relative rotation offset by moving the K-mirror
+    
+    Parameters:
+        delta_PA (float): rotation offset in degrees (+/- = E/W)
+    
+    Returns:
+        None
+    '''
         pass
 
     def autofocus(self):
         pass
     
     def focusoffset(self, delta_f=None):
-        '''
-        Apply a relative telescope focus offset        
-        
-        Parameters:
-            delta_f (float): focus offset value 
-        '''
+    '''
+    Apply a relative telescope focus offset        
+    
+    Parameters:
+        delta_f (float): focus offset value 
+    '''
         pass
     
     def focus(self, value=None, temp=None):
-        '''
-        Move focus to a particular value or a first guess given a temperature
-        
-        Parameters:
-            value (float): if provided, focus stage moves to this value
-            temp (float): if 'value' is not provided or 'value=None', focus stage moves to best guess based on focus vs temperature model
-        Returns:
-            None
-        '''
+    '''
+    Move focus to a particular value or a first guess given a temperature
+    
+    Parameters:
+        value (float): if provided, focus stage moves to this value
+        temp (float): if 'value' is not provided or 'value=None', focus stage moves to best guess based on focus vs temperature model
+    Returns:
+        None
+    '''
         pass
         
+    def track_on(self, derotator=True, rates=None):
+    '''
+    Turn on tracking, optionally track in rotation, optionally 
+    supply non-sidereal track rates.
+    '''
+        pass
+
+    def track_off(self):
+        pass
         
-        
-        
-        
-    
-    
-    
-    
