b0e8db54e9054ceb0a0818fa8e0a49a80e42e105
ndrory <40801533+ndrory@users.noreply.github.com>
2021-10-28 11:52:15 -0500

    add guide camera interface

---
+++ lvmops/agc_interface.py
@@ -0,0 +1,21 @@
+class agc(object):
+    def __init__(self, camera=None):
+        pass
+
+    def status(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def power_on(self):
+        pass
+
+    def power_off(self):
+        pass
+
+    def expose(self, exptime=0.0, imtype=None, Nexp=1):
+        pass
+
+    def bias(self, Nexp=1):
+        pass
