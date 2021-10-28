18c4eaa7774af486cf20f9574070974dd077a571
ndrory <40801533+ndrory@users.noreply.github.com>
2021-10-28 12:32:45 -0500

    add execute_obs()

--- lvmops/lvmi_interface.py
+++ lvmops/lvmi_interface.py
@@ -21,30 +21,36 @@ class lvmi(object):
       pass
 
     def setup_obs(self, obs=None):
-      '''
-      Point all telescopes to start a fine acquisition for a science observation and home the calibration telescope shutter
-    
-      Parameters:
-      obs (observation): object of class "observation" that contains RA,DEC,PA for science field, two sky fields, and RA,DEC list of calibration stars
-       
-      Returns:
-      None
-      '''
+    '''
+    Point all telescopes to start a fine acquisition for a science observation and home the calibration telescope shutter
+  
+    Parameters:
+    obs (observation): object of class "observation" that contains RA,DEC,PA for science field, two sky fields, and RA,DEC list of calibration stars
+      
+    Returns:
+    None
+    '''
       pass
     
     def aquire_obs(self, obs=None, guide=True, offsetcal=True, verify=False):
-      '''
-      Do fine aquisition for an observation by locking in science and sky telescope guidestars and 1st calibration star.
-      
-      Parameters:
-      obs (observation): object of class "observation" that contains RA,DEC and guide camera X,Y pixel coordinates for science and sky telescope guidestars and calibration stars
-      guide (bool): if True start guiding after fine aquisition is finished
-      offsetcal (bool): if True (default) apply offset to put 1st calibration star on 1st calibration fiber. 
-      verify (bool): if True output aquisition diagnostics (e.g. astrometry residual plots) and ask for user approval to proceed
-      
-      Returns:
-      None
-      '''
+    '''
+    Do fine aquisition for an observation by locking in science and sky telescope guidestars and 1st calibration star.
+    
+    Parameters:
+    obs (observation): object of class "observation" that contains RA,DEC and guide camera X,Y pixel coordinates for science and sky telescope guidestars and calibration stars
+    guide (bool): if True start guiding after fine aquisition is finished
+    offsetcal (bool): if True (default) apply offset to put 1st calibration star on 1st calibration fiber. 
+    verify (bool): if True output aquisition diagnostics (e.g. astrometry residual plots) and ask for user approval to proceed
+    
+    Returns:
+    None
+    '''
+      pass
+
+    def execute_obs(self, obs=None):
+    '''
+    Execute observation described by obs object.
+    '''      
       pass
 
     def warmup_lamps(self, lamps=[]):
