d5ee8fb35c168e5a925312d90712c09b1671ceba
ndrory <40801533+ndrory@users.noreply.github.com>
2021-10-28 11:52:31 -0500

    add temperature sensors

--- lvmops/spec_interface.py
+++ lvmops/spec_interface.py
@@ -1,22 +1,15 @@
-class __sensors__(object):
-    def __init__(self):
-        pass
-
-    def t1(self):
-        pass
-
-    def t2(self):
-        pass
-
 class spec(object):
     def __init__(self):
         pass
 
-    def expose(self, exptime=0.0, shutter=True, imtype=None,Nexp=1):
+    def expose(self, exptime=0.0, shutter=True, imtype=None, Nexp=1):
         pass
 
-    def dark(self, exptime=0.0, imtype="dark",Nexp=1):
+    def dark(self, exptime=0.0, imtype="dark", Nexp=1):
         pass
 
-    def bias(self,Nexp=1):
+    def bias(self, Nexp=1):
+        pass
+
+    def get_temperature(self, sensor=None):
         pass
