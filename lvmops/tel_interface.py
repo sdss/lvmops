c7896209885137046ea8979deb0266db3f4a0c26
ndrory <40801533+ndrory@users.noreply.github.com>
2021-10-28 11:38:19 -0500

    add park, zenigh, altaz

--- lvmops/tel_interface.py
+++ lvmops/tel_interface.py
@@ -3,14 +3,25 @@ class telescope(object):
     def __init__(self, name="sci", address=None):
         pass
 
-    def goto(self, ra, dec, PA=0, target='optical_axis', deg=True):
+    def goto_park(self):
+    '''
+    Move telescope to safe park position.
+    '''
+        pass
+
+    def goto_zenith(self):
+        pass
+
+    def goto_eq(self, ra, dec, PA=0, target='optical_axis', deg=True):
+        pass
+
+    def goto_aa(self, alt, az, PA=0, target='optical_axis', deg=True):
         pass
 
     def goto_screen(self): 
         pass
 
     def offset(self, target=None, delta_ra=None, delta_dec=None, delta_x=None, delta_y=None):
-        
         pass
     
     def paoffset(self, delta_PA=None):
