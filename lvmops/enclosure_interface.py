b60a1b90b4b8b217619cdde646a354b990c26050
EvelynJ <evelynjohnston.astro@gmail.com>
2021-11-18 17:22:32 -0300

    adding further comments and notes

--- lvmops/enclosure_interface.py
+++ lvmops/enclosure_interface.py
@@ -6,29 +6,42 @@ class enclosure(object):
     
     def open(self):
     '''
-    Ensure telescopes are parked before opening dome
-    Error out if the dome safety interlock are not OK (i.e. people on telescope platform)
-    Error out if any calibration lamps are on
-    Error out if dome lights are on
+    Open the dome
+    
+    ======
+    Comments and desired actions:
+    - Ensure telescopes are parked before opening dome
+    - Error out if the dome safety interlock are not OK (i.e. people on telescope platform)
+    - Error out if any calibration lamps are on
+    - Error out if dome lights are on
     '''
         pass
 
     def close(self):
     '''
-    Ensure telescopes are parked before closing dome
-    Error out if dome safety interlock is not OK (i.e. people on telescope platform)
+    Close the dome
+
+    ======
+    Comments and desired actions:
+    - Close the enclosure
+    - Ensure telescopes are parked before closing dome
+    - Error out if dome safety interlock is not OK (i.e. people on telescope platform)
     '''
         pass
 
     def status(self):
     '''
-    Return readout of all sensors and dome position.
+    Return readout of all sensors, lights and dome position.
     '''
         pass
 
     def dome_lights_on(self):
     '''
     Turn dome lights on
+    
+    ======
+    Comments and desired actions:
+    - Error out if dome is open
     '''
         pass
 
