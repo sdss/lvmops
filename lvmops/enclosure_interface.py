7a01af1cfae47ddee13e0d24b0b860bb3460ab3c
EvelynJ <evelynjohnston.astro@gmail.com>
2021-11-24 15:16:22 -0300

    updated descriptions

--- lvmops/enclosure_interface.py
+++ lvmops/enclosure_interface.py
@@ -10,10 +10,14 @@ class enclosure(object):
     
     ======
     Comments and desired actions:
-    - Ensure telescopes are parked before opening dome
-    - Error out if the dome safety interlock are not OK (i.e. people on telescope platform)
-    - Error out if any calibration lamps are on
-    - Error out if dome lights are on
+    - Check if telescopes are parked before opening dome
+        - if telescopes not parked, check dome safety interlock
+            - if interlock is engaged, do not open dome and return error message
+            - if interlock is not engaged, park telescope and open dome when complete
+    - Check dome safety interlock:
+        - if interlock is engaged, do not open dome and return error message (assume people are inside dome)
+    - Check status of dome and calibration lamps
+        - if any lamps on, do not open dome. Return error message (assume calibrations running or people inside dome)
     '''
         pass
 
@@ -23,9 +27,12 @@ class enclosure(object):
 
     ======
     Comments and desired actions:
-    - Close the enclosure
-    - Ensure telescopes are parked before closing dome
-    - Error out if dome safety interlock is not OK (i.e. people on telescope platform)
+    - Check if telescopes are parked before closing dome
+        - if telescopes not parked, check dome safety interlock
+            - if interlock is engaged, do not close dome and return error message
+            - if interlock is not engaged, park telescope and close dome when complete
+    - Check dome safety interlock:
+        - if interlock is engaged, do not close dome. Return error message (assume people are inside dome)
     '''
         pass
 
@@ -41,12 +48,20 @@ class enclosure(object):
     
     ======
     Comments and desired actions:
-    - Error out if dome is open
+    - Run status (enclosure_interface) to check status of dome
+        - if open, do not turn on dome lights
+    - run status (LVMI_interface) to check instrument status
+        - if not idle (i.e. carrying out any observations or calibrations), do not turn on the dome lights
     '''
         pass
 
     def dome_lights_off(self):
     '''
     Turn dome lights off
+
+    ======
+    Comments and desired actions:
+    - Check dome safety interlock:
+        - if interlock is engaged, do not turn off the dome lights (assume people are inside dome)
     '''
         pass
